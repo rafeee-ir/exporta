4ba455f6f1b9f0cb3a08cb29442a5057833d0358
Hadi <rafeee.ir@gmail.com>
2023-07-21 20:55:37 +0330

    update css addresses

--- resources/views/layouts/dapp.blade.php
+++ resources/views/layouts/dapp.blade.php
@@ -6,27 +6,27 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>{{config('app.name')}}'s Dashboard | @yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="{{asset('storage/images/dfavicon.png')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/themify-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/metisMenu.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/slicknav.min.css')}}">
+    <link rel="shortcut icon" type="image/png" href="{{url('images/dfavicon.png')}}">
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{url('css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{url('css/metisMenu.css')}}">
+    <link rel="stylesheet" href="{{url('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{url('css/slicknav.min.css')}}">
     <!-- amchart css -->
-    <link rel="stylesheet" href="{{asset('storage/assets/css/export.css')}}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{url('css/export.css')}}" type="text/css" media="all" />
     <!-- Start datatable css -->
-    <link rel="stylesheet" type="text/css" href="{{asset('storage/assets/css/jquery.dataTables.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('storage/assets/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('storage/assets/css/responsive.bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('storage/assets/css/responsive.jqueryui.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/jquery.dataTables.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/responsive.bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/responsive.jqueryui.min.css')}}">
     <!-- others css -->
-    <link rel="stylesheet" href="{{asset('storage/assets/css/typography.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/default-css.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/styles.css')}}">
-    <link rel="stylesheet" href="{{asset('storage/assets/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{url('css/typography.css')}}">
+    <link rel="stylesheet" href="{{url('css/default-css.css')}}">
+    <link rel="stylesheet" href="{{url('css/styles.css')}}">
+    <link rel="stylesheet" href="{{url('css/responsive.css')}}">
     <!-- modernizr css -->
-    <script src="{{asset('storage/assets/js/vendor/modernizr-2.8.3.min.js')}}"></script>
+    <script src="{{url('js/vendor/modernizr-2.8.3.min.js')}}"></script>
 
 
 
@@ -71,14 +71,14 @@
 @include('dashboard.offset')
 <!-- offset area end -->
 <!-- jquery latest version -->
-<script src="{{asset('storage/assets/js/vendor/jquery-2.2.4.min.js')}}"></script>
+<script src="{{url('js/vendor/jquery-2.2.4.min.js')}}"></script>
 <!-- bootstrap 4 js -->
-<script src="{{asset('storage/assets/js/popper.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/owl.carousel.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/metisMenu.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/jquery.slimscroll.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/jquery.slicknav.min.js')}}"></script>
+<script src="{{url('js/popper.min.js')}}"></script>
+<script src="{{url('js/bootstrap.min.js')}}"></script>
+<script src="{{url('js/owl.carousel.min.js')}}"></script>
+<script src="{{url('js/metisMenu.min.js')}}"></script>
+<script src="{{url('js/jquery.slimscroll.min.js')}}"></script>
+<script src="{{url('js/jquery.slicknav.min.js')}}"></script>
 
 {{--<!-- start chart js -->--}}
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>--}}
@@ -91,18 +91,18 @@
 {{--    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];--}}
 {{--</script>--}}
 <!-- all line chart activation -->
-<script src="{{asset('storage/assets/js/line-chart.js')}}"></script>
+<script src="{{url('js/line-chart.js')}}"></script>
 <!-- all pie chart -->
-<script src="{{asset('storage/assets/js/pie-chart.js')}}"></script>
+<script src="{{url('js/pie-chart.js')}}"></script>
 <!-- Start datatable js -->
-<script src="{{asset('storage/assets/js/jquery.dataTables.js')}}"></script>
-<script src="{{asset('storage/assets/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{asset('storage/assets/js/responsive.bootstrap.min.js')}}"></script>
+<script src="{{url('js/jquery.dataTables.js')}}"></script>
+<script src="{{url('js/jquery.dataTables.min.js')}}"></script>
+<script src="{{url('js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{url('js/dataTables.responsive.min.js')}}"></script>
+<script src="{{url('js/responsive.bootstrap.min.js')}}"></script>
 <!-- others plugins -->
-<script src="{{asset('storage/assets/js/plugins.js')}}"></script>
-<script src="{{asset('storage/assets/js/scripts.js')}}"></script>
+<script src="{{url('js/plugins.js')}}"></script>
+<script src="{{url('js/scripts.js')}}"></script>
 @yield('script')
 </body>
 
